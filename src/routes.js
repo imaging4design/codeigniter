569aa19fa5f9e98d727e1478a35908933baeba34
Gavin Lovegrove <gavinlovegrove@Gavins-MacBook-Pro.local>
2018-12-25 09:51:20 +1300

    Test push

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 import ListAgeGroups from './ListAgeGroups';
 import ListRecordType from './ListRecordType';
-
+// array of routes
 export const routes = [
 	{ path: '', component: ListAgeGroups },
 	{ path: '/test', component: ListRecordType }
