f9271e983b45ed89b8c9973cefd07a0d5a0a3642
Gavin Lovegrove <gavin@imaging4design.co.nz>
2019-01-15 11:34:34 +1300

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -380,11 +380,12 @@ $config['encryption_key'] = '3904d962025f20f11c8796780902953d';
 | except for 'cookie_prefix' and 'cookie_httponly', which are ignored here.
 |
 */
-$config['sess_driver'] = 'files';
+//$config['sess_driver'] = 'files';
+$config['sess_driver'] = 'database';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 7200;
-//$config['sess_save_path'] = NULL;
-$config['sess_save_path'] = APPPATH . 'sessions/'; 
+//$config['sess_save_path'] = APPPATH . 'sessions/';
+$config['sess_save_path'] = 'ci_sessions';
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
